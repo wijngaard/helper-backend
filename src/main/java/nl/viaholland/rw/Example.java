8c5c38a5dc39591683330775bca5f78dfe2c74c1
Renny Wijngaard <renny@Rennys-Air.home>
2016-11-30 21:56:34 +0100

    Adding spring-boot 1.4.2 in the pom and adding an Example.class

---
+++ src/main/java/nl/viaholland/rw/Example.java
@@ -0,0 +1,25 @@
+package nl.viaholland.rw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by renny on 30/11/2016.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
+
