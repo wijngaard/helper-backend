2c1d5df0e839f3df6990ace99367f7ba7f53ffce
Marc Enschede <marc@Marcs-MacBook-Pro-2.local>
2017-05-03 19:11:08 +0200

    Moved to let 2 apps work together in Docker

---
+++ app1/src/main/java/nl/marcenschede/tests/ReleaseApplication.java
@@ -0,0 +1,12 @@
+package nl.marcenschede.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseApplication.class, args);
+	}
+}
